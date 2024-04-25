ca4abdf306796b4ade4c9a41b5050f5e19efce12
JordanClough <jordan_clough@sfu.ca>
2024-04-25 14:00:19 -0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/{nextjs-dashboard}' : '',
+  };
+  
+  //module.exports = nextConfig;
+  export default nextConfig;
